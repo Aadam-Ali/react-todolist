160db2e7da03c3a890185729f4bec4fde1a6ba1e
Aadam Ali <ali.aadam@outlook.com>
2021-07-24 22:48:57 +0100

    fixed localstorage is null  issue

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,6 +6,8 @@ const TodoList = ({ todos, setTodos }) => {
   useEffect(() => { setTodos(JSON.parse(localStorage.getItem("todos"))) }, [])
   useEffect(() => { localStorage.setItem("todos", JSON.stringify(todos)) }, [todos])
 
+  if (!localStorage.setItem("todos", JSON.stringify(todos))) {return}
+
   return (
     <div className="todo-container">
       {todos.map((todo) => (<TodoItem key={todo.id} text={todo.text} todos={todos} setTodos={setTodos} todo={todo} />))}
